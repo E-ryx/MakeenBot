30f038689c444e5b3ee037ae41ba0c5718cf7d3f
MoEain <meem.eain@gmail.com>
2025-04-13 14:01:40 +0330

    Create BotConfig.cs

---
+++ Models/BotConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MakeenBot.Models;
+
+public class BotConfig
+{
+    public string Token { get; set; }
+    public string BaleApi { get; set; }
+    public string Webhook { get; set; }
+}
