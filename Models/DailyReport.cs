631bb5453409c258cd4f4b756e2f9412e10b6c53
MoEain <meem.eain@gmail.com>
2025-04-13 14:01:43 +0330

    Create DailyReport.cs

---
+++ Models/DailyReport.cs
@@ -0,0 +1,11 @@
+﻿namespace MakeenBot.Models
+{
+    public class DailyReport
+    {
+        public string? PersianDate { get; set; }
+        public string? NameTag { get; set; }
+        public int? ReportNumber { get; set; }
+        public int? WorkHour { get; set; }
+
+    }
+}
